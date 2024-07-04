2b3bd66d46da6980fc862fd00f72ff4b40a5e31d
Vano <vano50@mail.ru>
2024-07-04 11:25:57 +0300

    Initial commit

---
+++ home-2-7-2/home-2-7-2.cpp
@@ -0,0 +1,15 @@
+﻿#define SUB(a, b) (a - (b))
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
